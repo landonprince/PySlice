6358c362a9db448e9eca761a19e43183852c03f9
landonprince <landoncprince@gmail.com>
2024-05-19 00:54:02 -0500

    v1 - movement

--- game.py
+++ game.py
@@ -1,16 +1,69 @@
 import sys
 import pygame
+from scripts.entities import PhysicsEntity
+from scripts.utils import load_image, load_images
+from scripts.tilemap import Tilemap
 
-pygame.init()
 
-screen = pygame.display.set_mode((640, 480))
+class Game:
+    def __init__(self):
+        pygame.init()
 
-clock = pygame.time.Clock()
+        pygame.display.set_caption('PySlice')
+        self.screen = pygame.display.set_mode((640, 480))
+        self.display = pygame.Surface((320, 240))
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
+        self.clock = pygame.time.Clock()
 
-    pygame.display.update()
-    clock.tick(60)
+        self.movement = [False, False]
+
+        self.assets = {
+            'decor': load_images('tiles/decor'),
+            'grass': load_images('tiles/grass'),
+            'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images('tiles/stone'),
+            'player': load_image('entities/player.png'),
+        }
+
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+
+        self.tilemap = Tilemap(self, tile_size=16)
+
+        self.scroll = [0, 0]
+
+    def run(self):
+        while True:
+            self.display.fill((14, 219, 248))
+
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[0] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[0]) / 30
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.tilemap.render(self.display, offset=render_scroll)
+
+            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display, offset=render_scroll)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                        self.movement[1] = True
+                    if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
+                        self.player.velocity[1] = -3
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                        self.movement[1] = False
+
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+            pygame.display.update()
+            self.clock.tick(60)
+
+
+Game().run()
